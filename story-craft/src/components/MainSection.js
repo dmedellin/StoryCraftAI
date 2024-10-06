0cacc3b60da0a0e9e7869e2e46765c66ce349a59
dmedellin <davidjmedellin@gmail.com>
2024-10-06 15:27:12 -0400

    Add initial project files and structure

---
+++ story-craft/src/components/MainSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+
+function MainSection() {
+  return (
+    <Container style={{ marginTop: '20px' }}>
+      {/* Future content will go here */}
+    </Container>
+  );
+}
+
+export default MainSection;
